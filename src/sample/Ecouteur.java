02177098ffdd91149818ecb85ccbad21ec2ff211
mathieus1u <hugo.mathieu-steinbach5@etu.univ-lorraine.fr>
2021-01-30 11:23:56 +0100

    Finalisation du TP.

--- src/sample/Ecouteur.java
+++ src/sample/Ecouteur.java
@@ -2,32 +2,39 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Ecouteur implements EventHandler<ActionEvent> {
 
     private int nombreCliques;
     private final Label labelNombreCliques;
-    private final Label des1;
-    private final Label des2;
-    private final Label des3;
+    private final ArrayList<Button> des = new ArrayList<>();
+    private final AtomicBoolean[] desBloque = new AtomicBoolean[3];
     private int nombreDe421;
     private final Label labelNombreDe421;
+    private final int[] lancers = new int[3];
 
-    public Ecouteur(Label des1, Label des2, Label des3, Label nombreCliques, Label nombreDe421) {
+    public Ecouteur(ArrayList<Button> des, Label nombreCliques, Label nombreDe421, AtomicBoolean[] desBloque) {
         this.nombreCliques = 0;
         this.labelNombreCliques = nombreCliques;
-        this.des1 = des1;
-        this.des2 = des2;
-        this.des3 = des3;
+        this.des.addAll(des);
         this.nombreDe421 = 0;
         this.labelNombreDe421 = nombreDe421;
+        for (int i = 0; i < 3; ++i) {
+            this.desBloque[i] = desBloque[i];
+            this.lancers[i] = 1;
+        }
     }
 
     @Override
     public void handle(ActionEvent event) {
+        // Si on a pas encore joué, on affiche un message de bienvenu
         if (nombreCliques == 0) {
             System.out.println("Bienvenue !\n");
         }
@@ -38,82 +45,30 @@ public class Ecouteur implements EventHandler<ActionEvent> {
         labelNombreCliques.setText("Nombre de cliques : " + nombreCliques + " ");
         labelNombreDe421.setText("Nombre de 421 : " + nombreDe421);
 
-        int lancer1 = (int) (Math.random() * 6 + 1);
-        int lancer2 = (int) (Math.random() * 6 + 1);
-        int lancer3 = (int) (Math.random() * 6 + 1);
+        // On lance les dés.
+        for (int i = 0; i < 3; i++) {
+            if (!desBloque[i].get()) {
+                lancers[i] = (int) (Math.random() * 6 + 1);
+            }
+        }
 
-        if ((lancer1 == 1 && lancer2 == 2 && lancer3 == 4) || (lancer1 == 1 && lancer2 == 4 && lancer3 == 2) || (lancer1 == 2 && lancer2 == 1 && lancer3 == 4) || (lancer1 == 2 && lancer2 == 4 && lancer3 == 1) || (lancer1 == 4 && lancer2 == 2 && lancer3 == 1) || (lancer1 == 4 && lancer2 == 1 && lancer3 == 2)) {
+        // On regarde s'il y a un 421.
+        if ((lancers[0] == 1 && lancers[1] == 2 && lancers[2] == 4) || (lancers[0] == 1 && lancers[1] == 4 && lancers[2] == 2) || (lancers[0] == 2 && lancers[1] == 1 && lancers[2] == 4) || (lancers[0] == 2 && lancers[1] == 4 && lancers[2] == 1) || (lancers[0] == 4 && lancers[1] == 2 && lancers[2] == 1) || (lancers[0] == 4 && lancers[1] == 1 && lancers[2] == 2)) {
             nombreDe421++;
         }
 
+        // On charge les images des dés.
         Image face1 = new Image(getClass().getResourceAsStream("../ressources/1.png"), 50, 50, true, true);
         Image face2 = new Image(getClass().getResourceAsStream("../ressources/2.png"), 50, 50, true, true);
         Image face3 = new Image(getClass().getResourceAsStream("../ressources/3.png"), 50, 50, true, true);
         Image face4 = new Image(getClass().getResourceAsStream("../ressources/4.png"), 50, 50, true, true);
         Image face5 = new Image(getClass().getResourceAsStream("../ressources/5.png"), 50, 50, true, true);
         Image face6 = new Image(getClass().getResourceAsStream("../ressources/6.png"), 50, 50, true, true);
+        Image[] faces = {face1, face2, face3, face4, face5, face6};
 
-        switch (lancer1) {
-            case 1:
-                des1.setGraphic(new ImageView(face1));
-                break;
-            case 2:
-                des1.setGraphic(new ImageView(face2));
-                break;
-            case 3:
-                des1.setGraphic(new ImageView(face3));
-                break;
-            case 4:
-                des1.setGraphic(new ImageView(face4));
-                break;
-            case 5:
-                des1.setGraphic(new ImageView(face5));
-                break;
-            case 6:
-                des1.setGraphic(new ImageView(face6));
-                break;
-        }
-
-        switch (lancer2) {
-            case 1:
-                des2.setGraphic(new ImageView(face1));
-                break;
-            case 2:
-                des2.setGraphic(new ImageView(face2));
-                break;
-            case 3:
-                des2.setGraphic(new ImageView(face3));
-                break;
-            case 4:
-                des2.setGraphic(new ImageView(face4));
-                break;
-            case 5:
-                des2.setGraphic(new ImageView(face5));
-                break;
-            case 6:
-                des2.setGraphic(new ImageView(face6));
-                break;
-        }
-
-        switch (lancer3) {
-            case 1:
-                des3.setGraphic(new ImageView(face1));
-                break;
-            case 2:
-                des3.setGraphic(new ImageView(face2));
-                break;
-            case 3:
-                des3.setGraphic(new ImageView(face3));
-                break;
-            case 4:
-                des3.setGraphic(new ImageView(face4));
-                break;
-            case 5:
-                des3.setGraphic(new ImageView(face5));
-                break;
-            case 6:
-                des3.setGraphic(new ImageView(face6));
-                break;
+        // On applique les bonnes images au dés.
+        for (int i = 0; i < 3; i++) {
+            des.get(i).setGraphic(new ImageView(faces[lancers[i] - 1]));
         }
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,35 +13,54 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
 
+        // On définit le titre de la fenêtre.
         primaryStage.setTitle("Jeu 421");
 
+        // On définit le BorderPane.
         BorderPane root = new BorderPane();
         primaryStage.setScene(new Scene(root, 300, 275));
 
+        // On s'occupe du bouton jouer.
         Button boutonJouer = new Button("Jouer");
         root.setBottom(boutonJouer);
         Image face1 = new Image(getClass().getResourceAsStream("../ressources/1.png"), 50, 50, true, true);
-        Label des1 = new Label("");
-        Label des2 = new Label("");
-        Label des3 = new Label("");
-        des1.setGraphic(new ImageView(face1));
-        des2.setGraphic(new ImageView(face1));
-        des3.setGraphic(new ImageView(face1));
+        ArrayList<Button> des = new ArrayList<>(3);
+        AtomicBoolean[] desBloque = new AtomicBoolean[3];
+        for (int i = 0; i < 3; i++) {
+            des.add(new Button());
+            desBloque[i] = new AtomicBoolean(false);
+            int finalI = i;
+            des.get(i).setOnAction(e -> {
+                desBloque[finalI].set(!desBloque[finalI].get());
+                if (desBloque[finalI].get()) {
+                    System.out.println("Vous avez bloqué le dé "+ (finalI+1) +" !");
+                } else {
+                    System.out.println("Vous avez débloqué le dé " + (finalI+1) +" !");
+                }
+            });
+            des.get(i).setGraphic(new ImageView(face1));
+        }
+        // On gère les affichage des compteurs.
         Label nombreClique = new Label("Nombre de cliques : 0 ");
         Label nombreDe421 = new Label("Nombre de 421 : 0");
-        boutonJouer.setOnAction(new Ecouteur(des1, des2, des3, nombreClique, nombreDe421));
+        boutonJouer.setOnAction(new Ecouteur(des, nombreClique, nombreDe421, desBloque));
 
+        // On gère le bouton quitter.
         Button boutonQuitter = new Button("");
         Image image = new Image(getClass().getResourceAsStream("../ressources/quitter.png"), 35, 35, true, true);
         ImageView icon = new ImageView(image);
-        boutonQuitter.setGraphic(icon) ;
-        boutonQuitter.setOnAction(e ->Platform.exit());
+        boutonQuitter.setGraphic(icon);
+        boutonQuitter.setOnAction(e -> Platform.exit());
 
+        // On gère l'affichage des boutons dubas.
         HBox boutonsBas = new HBox();
         boutonsBas.getChildren().addAll(boutonJouer, boutonQuitter);
         boutonsBas.setAlignment(Pos.CENTER);
@@ -49,12 +68,14 @@ public class Main extends Application {
         boutonsBas.setPadding(new Insets(0, 0, 20, 0));
         root.setBottom(boutonsBas);
 
-        HBox des = new HBox();
-        des.getChildren().addAll(des1, des2, des3);
-        des.setAlignment(Pos.CENTER);
-        des.setSpacing(25);
-        root.setCenter(des);
+        // On gère l'affichage des dés.
+        HBox desBox = new HBox();
+        desBox.getChildren().addAll(des);
+        desBox.setAlignment(Pos.CENTER);
+        desBox.setSpacing(25);
+        root.setCenter(desBox);
 
+        // On gère l'affichage des compteurs.
         HBox compteurs = new HBox();
         compteurs.getChildren().addAll(nombreClique, nombreDe421);
         compteurs.setAlignment(Pos.CENTER);
